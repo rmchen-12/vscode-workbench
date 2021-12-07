fe9ac0a95ef5e8d822d13094eea5b327ec0c6386
rmchen-12 <rmchen1993@163.com>
2021-12-07 12:09:27 +0800

    chore: 删除遗留的文件

--- src/services/app/appService.ts
+++
@@ -1,9 +0,0 @@
-import { createDecorator } from '../../platform/instantiation/instantiation';
-
-export const IAppService = createDecorator<IAppService>('appService');
-
-export interface IAppService {
-  // service的标识符，有这个属性说明是一个service
-  readonly _serviceBrand: undefined;
-//   getUserInfo(): void;
-}
--- src/services/auth/authService.ts
+++
@@ -1,9 +0,0 @@
-import { createDecorator } from "../../platform/instantiation/instantiation";
-
-export const IAuthService = createDecorator<IAuthService>('authService');
-
-export interface IAuthService {
-  // service的标识符，有这个属性说明是一个service
-  readonly _serviceBrand: undefined;
-  getUserInfo(): void;
-}
